b8e91a7b18d50b3e8f9ea2f6ef89c44bd22f6ba0
Yacer Baba <yacerbaba10@gmail.com>
2024-01-14 18:17:55 +0100

    initial commit

---
+++ src/test/java/com/yacer/unilearn/UniLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yacer.unilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
